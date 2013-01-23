3dbf492dc1882fa8c181578e69af75ea9734dcbf
caok <caok1231@gmail.com>
2013-01-23 22:15:27 +0800

    change the show of event,add validate of end_date >= start_date

---
+++ app/helpers/event_helper.rb
@@ -0,0 +1,17 @@
+module EventHelper
+  def date_merge(event)
+    start = event.start_date
+    finish = event.end_date
+    text = I18n.localize(start, :format => format_year(start))
+    if start == finish
+      text
+    else
+      text + " - #{I18n.localize(finish, :format => format_year(finish))}" unless finish.nil?
+    end
+  end
+
+  private
+  def format_year(time)
+    (time.year == Time.zone.now.year) ? :stamp : :stamp_with_year
+  end
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -6,4 +6,15 @@ class Event < ActiveRecord::Base
   has_html_pipeline :content, :markdown
 
   validates :title, :start_date, :end_date, :presence => true
+  validate :end_date_must_after_start_date
+
+  scope :unfinished, where(["end_date > ? ", Date.current])
+
+  private
+  def end_date_must_after_start_date
+    if end_date.present? && start_date.present? && end_date < start_date
+      message = I18n.t('errors.messages.greater_than_or_equal_to', :count => Event.human_attribute_name(:start_date))
+      errors.add(:end_date, message)
+    end
+  end
 end
