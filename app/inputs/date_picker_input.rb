aafce1a8e3c46a6a087c173b8c78f10f0a874986
caok <caok1231@gmail.com>
2013-01-24 15:59:48 +0800

    add date_piceker_input

---
+++ app/inputs/date_picker_input.rb
@@ -0,0 +1,12 @@
+class DatePickerInput < SimpleForm::Inputs::Base
+  def input
+    template.content_tag(:div, :class => 'input-append') do
+      result = @builder.text_field(attribute_name,input_html_options)
+      result += template.content_tag(:span, :class => 'add-on') do
+        template.content_tag(:i, '', :class => 'icon-calendar datepicker-trigger')
+      end
+
+      result.html_safe
+    end
+  end
+end
