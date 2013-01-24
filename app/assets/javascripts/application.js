c877372b50b41095c1d1c6bf52b55a465662a545
caok <caok1231@gmail.com>
2013-01-24 16:45:20 +0800

    add bootstrap-datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,27 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require bootstrap-datepicker/core
+//= require bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN
 //= require jquery-fileupload/basic
 //= require_self
 
+$(function() {
+  var body = $('body');
+  var datepickerDefaults = {
+    language: 'zh-CN',
+    format: 'yyyy-mm-dd',
+    weekStart: '1'
+  };
+
+  body.on('click', '.datepicker-trigger', function() {
+    var el = $(this).closest('.date').find('.date_picker');
+    var datepicker = el.data('date_picker');
+    if (datepicker === null || datepicker === undefined) {
+      el.datepicker(datepickerDefaults).on('changeDate', function() {
+        el.datepicker('hide');
+      });
+    }
+
+    el.datepicker('show');
+  });
+});
--- app/inputs/date_picker_input.rb
+++ app/inputs/date_picker_input.rb
@@ -1,6 +1,6 @@
 class DatePickerInput < SimpleForm::Inputs::Base
   def input
-    template.content_tag(:div, :class => 'input-append') do
+    template.content_tag(:div, :class => 'input-append date') do
       result = @builder.text_field(attribute_name,input_html_options)
       result += template.content_tag(:span, :class => 'add-on') do
         template.content_tag(:i, '', :class => 'icon-calendar datepicker-trigger')
