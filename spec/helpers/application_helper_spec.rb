093e48abda76cb50df0c23eebb256e32258a96b4
caok <caok1231@gmail.com>
2013-02-05 17:08:16 +0800

    add application_helper_spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,5 +1,78 @@
+#encoding:utf-8
 require 'spec_helper'
 
 describe ApplicationHelper do
+  describe 'flash_class' do
+    subject { helper.flash_class(flash_key) }
+    context 'when flash key is :notice' do
+      let(:flash_key) { :notice }
+      it { should == 'alert-success' }
+    end
+    context 'when flash key is :error' do
+      let(:flash_key) { :error }
+      it { should == 'alert-error' }
+    end
+  end
 
+  describe 'render_close_icon' do
+    subject { helper.render_close_icon }
+    it { should have_selector('a.close') }
+  end
+
+  describe 'body_attributes' do
+    subject { helper.body_attributes }
+    context 'when user has signed in' do
+      before do
+        helper.should_receive(:user_signed_in?).with().and_return(true)
+      end
+      its([:class]) { should include("signed_in") }
+    end
+    context 'when user has not yet signed in' do
+      before do
+        helper.should_receive(:user_signed_in?).with().and_return(false)
+      end
+      its([:class]) { should include('signed_out') }
+    end
+  end
+
+  describe 'render_user_bar' do
+    def render
+      helper.render_user_bar
+    end
+    context 'when user has signed in' do
+      before do
+        helper.should_receive(:user_signed_in?).with().and_return(true)
+      end
+      it 'renders signed_in_user_bar' do
+        helper.should_receive(:render).with('signed_in_user_bar').once
+        render
+      end
+    end
+
+    context 'when user has not yet signed in' do
+      before do
+        helper.should_receive(:user_signed_in?).with().and_return(false)
+      end
+      it 'renders signed_out_user_bar' do
+        helper.should_receive(:render).with('signed_out_user_bar').once
+        render
+      end
+    end
+  end
+
+  describe 'render_flashes' do
+    context 'when it is called twice' do
+      it 'only renders flashes once' do
+        helper.should_receive(:render).with('flashes').once
+        helper.render_flashes
+      end
+    end
+  end
+
+  describe 'format_price' do
+    it "formatted price" do
+      price = 10000.12567
+      helper.format_price(price).should =~ /10,000.13元/
+    end
+  end
 end
